1d1f49b4d33a3d9e7f2be8e53de5f6dc4a10b1f2
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2022-09-09 21:25:39 +0600

    fifth commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://serverforlws.herokuapp.com/",
+    baseURL: "https://new-server-lws.herokuapp.com/",
 });
 
 export default axiosInstance;
